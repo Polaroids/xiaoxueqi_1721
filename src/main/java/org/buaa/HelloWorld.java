38ffa3db23646465627d944c43bf1198164db1da
unknown <lurongfu@buaa.edu.cn>
2019-08-22 11:46:19 +0800

    初始化

---
+++ src/main/java/org/buaa/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.buaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+* @SpringBootApplication
+* */
+@SpringBootApplication
+public class HelloWorld {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorld.class,args);
+    }
+}
